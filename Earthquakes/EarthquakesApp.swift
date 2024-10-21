8514ee27649ae585e417cebf52b044953e2a6437
kangddong <rkdehddud96@naver.com>
2024-10-22 00:09:13 +0900

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by 강동영 on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
